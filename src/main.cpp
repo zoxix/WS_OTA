e2e8db1e18b2d3ac5332da8fb0866c823bca25e7
zoxix <syafrudinrais@gmail.com>
2020-03-05 14:32:01 +0700

    ALALA

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+#include <Ticker.h>
+
+#define LED 2
+#define TEXTIFY(A) #A
+#define ESCAPEQUOTE(A) TEXTIFY(A)
+String buildTag = ESCAPEQUOTE(BUILD_TAG);
+WiFiManager wifiManager;
+
+void enableUpdateCheck();
+Ticker updateCheck(enableUpdateCheck, 30000); // timer for check update with interval 30s
+bool doUpdateCheck = true;
+
+void enableUpdateCheck()
+{
+  doUpdateCheck = true;
+}
+
+void DownloadBin()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    //==========================downloading firmware.bin with HTTP OTA================
+    t_httpUpdate_return ret = ESPhttpUpdate.update("http://ota.firmandev.tech/stable/firmware.php?tag=" + buildTag);
+    switch (ret)
+    {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("UPDATE ERROR (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println(" Already in Current Version");
+      break;
+    case HTTP_UPDATE_OK:
+      Serial.println("Updating FIrmware...");
+    }
+  }
+}
+
+void setup()
+{
+  // put your setup code
+  updateCheck.start();
+  wifiManager.autoConnect("DevOps");
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  updateCheck.update();
+  digitalWrite(LED, LOW);
+  delay(1000);
+  digitalWrite(LED, HIGH);
+  delay(500);
+
+  if (doUpdateCheck == true)
+  {
+    DownloadBin();
+    doUpdateCheck = false;
+  }
+}
